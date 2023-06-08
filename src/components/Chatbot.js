ea979f55d01ab5aceb0d97b1d1199b264477872f
adigulalkari <adigulalkari825@gmail.com>
2023-06-08 05:53:19 +0530

    Implemented Chatbot

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -2,6 +2,24 @@ import React from "react";
 import Navbar from "../components/Navbar"
 
 const Header=()=>{
+    
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const configuration = new Configuration({
+    apiKey: "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",
+    });
+    const openai = new OpenAIApi(configuration);
+
+    prompt = "You are talking to an elderly person. Talk with respect. Do not mention you are an AI. "
+
+    async function generateChatResponse(msg) {
+        const completion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: prompt+msg }],
+        });
+        return completion.data.choices[0].message;
+      }
+
     return (
         <div className="header-wraper">
             <Navbar/>
@@ -14,3 +32,6 @@ const Header=()=>{
 }
 
 export default Header
+
+
+generateChatResponse(msg);
