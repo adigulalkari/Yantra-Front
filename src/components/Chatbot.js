9df490a3af606962f2cde03d9996c5e30b7ed645
adigulalkari <adigulalkari825@gmail.com>
2023-06-08 11:08:49 +0530

    Finnaly fixed the chatbot

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -5,21 +5,31 @@ const Chatbot = () => {
   const { Configuration, OpenAIApi } = require("openai");
 
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   });
   const openai = new OpenAIApi(configuration);
 
-  const prompt = "You are talking to an elderly person. Talk with respect. Do not mention you are an AI. ";
+  const prompt = "You are talking to an elderly person. Talk with respect. Do not mention you are an AI.";
 
-  var [textInput, setTextInput] = useState(""); // State to hold the input value
-  var a="";
+  const [textInput, setTextInput] = useState(""); // State to hold the input value
+  const [response, setResponse] = useState(""); // State to hold the generated response
 
   const generateChatResponse = async () => {
+    const messages = [{ role: "user", content: prompt + textInput }];
+
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt + textInput }],
+      messages: messages.map((message) => ({
+        role: message.role,
+        content: message.content
+      })),
     });
-    a = completion.data.choices[0].message;
+
+    const generatedMessages = completion.data.choices.map((choice) => choice.message.content);
+    const response = generatedMessages.join("\n"); // Join the messages with a newline character
+
+    console.log(response); // Log the generated response to the console
+    setResponse(response); // Store the generated response in the state variable
   };
 
   const handleInputChange = (e) => {
@@ -28,11 +38,11 @@ const Chatbot = () => {
 
   return (
     <div className="header-wraper" style={{ background: "radial-gradient(50% 50% at 50% 50%, #DAAC78 0%, rgba(218, 172, 120, 0) 100%)" }}>
-      <div className="card-control" style={{ "padding-top": "3rem" }}>
+      <div className="card-control" style={{ paddingTop: "3rem" }}>
         <input id="text-desired1" type="text" style={{ height: "5rem" }} value={textInput} onChange={handleInputChange} />
         <button onClick={generateChatResponse}>Submit</button>
-        <input id="text-desired1" type="text" style={{ height: "5rem" }} placeholder={a} />
       </div>
+      <p>{response}</p> {/* Render the generated response */}
     </div>
   );
 };
